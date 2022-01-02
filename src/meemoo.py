799def7149d74cb44adf76b873e697efceed6d90
Mike Rosseel <mike.rosseel@gmail.com>
2022-01-02 18:40:58 +0100

    move models

--- src/meemoo.py
+++ src/meemoo.py
@@ -13,22 +13,37 @@ import vol_stats
 import tape_alert
 import tape_usage
 import tape_cap
+import threading, time
+
+    # print(write_error.text_to_write_error(write_err))
+    # print(read_error.text_to_read_error(read_err))
+    # print(non_med_err.text_to_non_med_error_count(non_med_err_str))
+    # print(seq_access.text_to_seq_access(seq_access_str))
+    # print(dev_stats.text_to_dev_stats(dev_stats_str))
+    # print(vol_stats.from_text(vol_stats_str))
+    # print(tape_alert.from_text(tape_alert_str))
+    # print(tape_usage.from_text(tape_usage_str))
+    # print(tape_cap.from_text(tape_cap_str))
+
 
 def process(interval, devices):
-    print("bla", interval, devices)
-    write_err, read_err, non_med_err_str, seq_access_str, dev_stats_str, vol_stats_str, tape_alert_str, tape_usage_str, tape_cap_str = sglogs_splitter.split_sg_output(text)
-    print(write_error.text_to_write_error(write_err))
-    print(read_error.text_to_read_error(read_err))
-    print(non_med_err.text_to_non_med_error_count(non_med_err_str))
-    print(seq_access.text_to_seq_access(seq_access_str))
-    print(dev_stats.text_to_dev_stats(dev_stats_str))
-    print(vol_stats.from_text(vol_stats_str))
-    print(tape_alert.from_text(tape_alert_str))
-    print(tape_usage.from_text(tape_usage_str))
-    print(tape_cap.from_text(tape_cap_str))
+    logging.info(f"Starting Meemoo monitoring with {interval} second interval, with devices: {devices}")
+
+    ticker = threading.Event()
+    while not ticker.wait(interval):
+        periodicTask(devices)
 
 
 
+def periodicTask(devices):
+    print("This loops on a timer every %d seconds", devices)
+    write_err, read_err, non_med_err_str, seq_access_str, dev_stats_str, vol_stats_str, tape_alert_str, tape_usage_str, tape_cap_str = get_log_classes(devices)
+
+
+def get_log_classes(devices):
+    for device in devices:
+        devicelog = 
+        return sglogs_splitter.split_sg_output(text)
 
 if __name__ == "__main__":
     logger = logging.getLogger()
@@ -67,4 +82,4 @@ if __name__ == "__main__":
     # assert os.path.exists(args.datadir), "datadir does not exist"
     # assert os.path.exists(args.resultdir), "resultdir does not exist"
     # assert os.path.exists(args.fitsdir), "fitsdir does not exist"
-    process(args.interval, args.devices)
+    process(int(args.interval), args.devices)
