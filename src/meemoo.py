a1744232c898621ab400a168765306e315b8c9ca
Mike Rosseel <mike.rosseel@gmail.com>
2022-01-27 17:31:41 +0100

    further db refinements

--- src/meemoo.py
+++ src/meemoo.py
@@ -39,13 +39,13 @@ def recreate_tables(conn):
     cur.execute("DROP TABLE IF EXISTS drive")
     cur.execute("DROP TABLE IF EXISTS tape")
     cur.execute("CREATE TABLE drive (id serial PRIMARY KEY, log_timestamp TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,   \
-            lifetime_media_loads INTEGER, lifetime_cleaning_operations INTEGER, lifetime_power_on_hours INTEGER, lifetime_media_motion_head_hours INTEGER, \
+            device VARCHAR, lifetime_media_loads INTEGER, lifetime_cleaning_operations INTEGER, lifetime_power_on_hours INTEGER, lifetime_media_motion_head_hours INTEGER, \
             lifetime_metres_of_tape_processed INTEGER, lifetime_media_motion_head_hours_when_incompatible_media_last_loaded INTEGER, \
             lifetime_power_on_hours_when_last_temperature_condition_occurred INTEGER, lifetime_power_on_hours_when_last_power_consumption_condition_occurred INTEGER, \
             media_motion_head_hours_since_last_successful_cleaning_operation INTEGER, media_motion_head_hours_since_2nd_to_last_successful_cleaning INTEGER, \
             media_motion_head_hours_since_3rd_to_last_successful_cleaning INTEGER, lifetime_power_on_hours_when_last_operator_initiated_forced_reset_and_or_emergency_eject_occurred INTEGER, \
             accumulated_transitions_to_idle_a INTEGER, non_medium_error_count INTEGER, dev_stats JSONB, read_error JSONB, write_error JSONB)")
-    cur.execute("CREATE TABLE tape (id serial PRIMARY KEY, log_timestamp TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,   \
+    cur.execute("CREATE TABLE tape (id serial PRIMARY KEY, log_timestamp TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP, drive_id INTEGER,  \
                 volume_serial_number TEXT,  tape_lot_identifier TEXT, volume_barcode TEXT, volume_manufacturer TEXT, volume_license_code TEXT, volume_personality TEXT, page_valid INTEGER, thread_count INTEGER, \
                 volstats JSONB, tapealert JSONB, tapeusage JSONB, tapecap JSONB, sequentialaccess JSONB)")
     cur.close()
@@ -61,23 +61,24 @@ def periodicTask(devices, logpages_getter, conn):
 def write_to_db(logs_per_device_dict, conn):
     logging.debug("writing to db ...")
     for device in logs_per_device_dict:
-        write_to_tape_db(logs_per_device_dict[device], conn)
-        write_to_drive_db(logs_per_device_dict[device], conn)
+        drive_id = write_to_drive_db(logs_per_device_dict[device], device, conn)
+        write_to_tape_db(logs_per_device_dict[device], drive_id, conn)
+
         # for key in logs_per_device_dict[device]:
         #     print("key:", key, logs_per_device_dict[device][key], '\n\n')
 
-def write_to_tape_db(logs, conn):
+def write_to_tape_db(logs, drive_id, conn):
     cur = conn.cursor()
     volStats = logs['vol_stats']
     logging.debug(f"Vol_stats: {logs['vol_stats'].to_json()}")
-    cur.execute(f"INSERT INTO tape (volume_serial_number, tape_lot_identifier, volume_barcode, volume_manufacturer, volume_license_code, \
+    cur.execute(f"INSERT INTO tape (drive_id, volume_serial_number, tape_lot_identifier, volume_barcode, volume_manufacturer, volume_license_code, \
                 volume_personality, page_valid, thread_count, volstats, tapealert, \
-                tapeusage, tapecap, sequentialaccess) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                (volStats.volume_serial_number, volStats.tape_lot_identifier, volStats.volume_barcode, volStats.volume_manufacturer, volStats.volume_license_code, volStats.volume_personality, volStats.page_valid, volStats.thread_count, logs['vol_stats'].to_json(), logs['tape_alert'].to_json(),logs['tape_usage'].to_json(),logs['tape_cap'].to_json(), logs['seq_access'].to_json()))
+                tapeusage, tapecap, sequentialaccess) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                (drive_id, volStats.volume_serial_number, volStats.tape_lot_identifier, volStats.volume_barcode, volStats.volume_manufacturer, volStats.volume_license_code, volStats.volume_personality, volStats.page_valid, volStats.thread_count, logs['vol_stats'].to_json(), logs['tape_alert'].to_json(),logs['tape_usage'].to_json(),logs['tape_cap'].to_json(), logs['seq_access'].to_json()))
     cur.close()
     conn.commit()
 
-def write_to_drive_db(logs, conn):
+def write_to_drive_db(logs, device, conn):
     cur = conn.cursor()
     devStats: DevStats = logs['dev_stats']
     writeError: WriteError = logs['write_err']
@@ -85,20 +86,22 @@ def write_to_drive_db(logs, conn):
     powerConditions: PowerConditions = logs['power_conditions']
     nonMedErrorCount: NonMedErrorCount = logs['non_med_err']
     logging.debug(f"Dev_stats: {devStats.to_json()}")
-    cur.execute(f"INSERT INTO drive (lifetime_media_loads, lifetime_cleaning_operations, lifetime_power_on_hours, lifetime_media_motion_head_hours, \
+    cur.execute(f"INSERT INTO drive (device, lifetime_media_loads, lifetime_cleaning_operations, lifetime_power_on_hours, lifetime_media_motion_head_hours, \
             lifetime_metres_of_tape_processed, lifetime_media_motion_head_hours_when_incompatible_media_last_loaded, \
             lifetime_power_on_hours_when_last_temperature_condition_occurred, lifetime_power_on_hours_when_last_power_consumption_condition_occurred, \
             media_motion_head_hours_since_last_successful_cleaning_operation, media_motion_head_hours_since_2nd_to_last_successful_cleaning, \
             media_motion_head_hours_since_3rd_to_last_successful_cleaning, lifetime_power_on_hours_when_last_operator_initiated_forced_reset_and_or_emergency_eject_occurred, \
-            accumulated_transitions_to_idle_a, non_medium_error_count, dev_stats, read_error, write_error) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-            (devStats.lifetime_media_loads, devStats.lifetime_cleaning_operations, devStats.lifetime_power_on_hours, devStats.lifetime_media_motion_head_hours,
+            accumulated_transitions_to_idle_a, non_medium_error_count, dev_stats, read_error, write_error) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING ID",
+            (device, devStats.lifetime_media_loads, devStats.lifetime_cleaning_operations, devStats.lifetime_power_on_hours, devStats.lifetime_media_motion_head_hours,
              devStats.lifetime_metres_of_tape_processed, devStats.lifetime_media_motion_head_hours_when_incompatible_media_last_loaded,
              devStats.lifetime_power_on_hours_when_last_temperature_condition_occurred, devStats.lifetime_power_on_hours_when_last_power_consumption_condition_occurred,
              devStats.media_motion_head_hours_since_last_successful_cleaning_operation, devStats.media_motion_head_hours_since_2nd_to_last_successful_cleaning,
              devStats.media_motion_head_hours_since_3rd_to_last_successful_cleaning, devStats.lifetime_power_on_hours_when_last_operator_initiated_forced_reset_and_or_emergency_eject_occurred,
              powerConditions.accumulated_transitions_to_idle_a, nonMedErrorCount.non_medium_error_count, devStats.to_json(), readError.to_json(), writeError.to_json()))
+    id_of_new_row = cur.fetchone()[0]
     cur.close()
     conn.commit()
+    return id_of_new_row
 
 
 if __name__ == "__main__":
