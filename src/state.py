9900229184ff79021cedb83b0d5e66367200dcb3
Mike Rosseel <mike.rosseel@gmail.com>
2022-02-15 16:16:27 +0100

    fix session bug

--- src/state.py
+++ src/state.py
@@ -4,9 +4,15 @@ from typing import Dict
 
 class State:
     def __init__(self):
+        # HASH dicts
         self.drive_hash_dict: Dict[str, str] = defaultdict(None)
         self.tape_hash_dict: Dict[str, str] = defaultdict(None)
+
+        # Device id dicts
+
+        # Device.id -> session_id
         self.session_dict: Dict[str, int] = defaultdict(None)
+        # Device.id -> volume_id
         self.session_serial_dict: Dict[str, str] = defaultdict(None)
 
     # returns (prev_hash, current_hash, changed?)
@@ -23,11 +29,13 @@ class State:
         adict[drive] = hash
         return "empty", hash, True
 
-    def get_session_id(self, device, volume_serial_number):
+    # returns the session id for current drive
+    def get_session_id(self, device_id, volume_serial_number):
         # if we don't have a serial number for this device, assign it this one - we might miss tape changes when the monitoring is not running (TODO)
-        if self.session_serial_dict.get(device) is None:
-            self.session_serial_dict[device] = volume_serial_number
-        if volume_serial_number != self.session_serial_dict[device]:
-            self.session_dict[device] = self.session_dict[device] + 1
-        return self.session_dict[device]
+        if self.session_serial_dict.get(device_id) is None:
+            self.session_serial_dict[device_id] = volume_serial_number
+        if volume_serial_number != self.session_serial_dict[device_id]:
+            self.session_dict[device_id] = self.session_dict[device_id] + 1
+            self.session_serial_dict[device_id] = volume_serial_number
+        return self.session_dict[device_id]
 
